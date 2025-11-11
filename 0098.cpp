da2830c4299298bbfbfa435a7191fe1ea050273c
l-library <borui-li@outlook.com>
2025-11-11 23:48:02 +0800

    2025/11/11

---
+++ 0098.cpp
@@ -0,0 +1,35 @@
+#include <climits>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    bool helper(TreeNode *root, long long &prev)
+    {
+        if (root == nullptr)
+            return true;
+        if (!helper(root->left, prev))
+            return false;
+        if (root->val <= prev)
+            return false;
+        prev = root->val;
+        return helper(root->right, prev);
+    }
+
+    bool isValidBST(TreeNode *root)
+    {
+        long long prev = LLONG_MIN;
+        return helper(root, prev);
+    }
+};
