1883c2b2b8e1290ebdaff169dce4bde847333f1c
l-library <borui-li@outlook.com>
2025-11-13 23:13:01 +0800

    2025/11/13

---
+++ 0226.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *invertTree(TreeNode *root)
+{
+    if (root == nullptr)
+        return root;
+    if (root->left != nullptr)
+        invertTree(root->left);
+    if (root->right != nullptr)
+        invertTree(root->right);
+    swap(root->left, root->right);
+    return root;
+}
