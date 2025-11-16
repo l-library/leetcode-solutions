72ce97ce5c69af308a91beb0bcdda94e4e3510cf
l-library <borui-li@outlook.com>
2025-11-16 23:14:09 +0800

    2025/11/16

--- 0101.cpp
+++ 0101.cpp
@@ -13,34 +13,15 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+bool helper(TreeNode *left, TreeNode *right)
+{
+    if (!left && !right)
+        return true;
+    if (!left || !right)
+        return false;
+    return left->val == right->val && helper(left->left, right->right) && helper(left->right, right->left);
+}
 bool isSymmetric(TreeNode *root)
 {
-    if (root == nullptr)
-        return true;
-    queue<TreeNode *> single_line; // 广度优先遍历
-    single_line.push(root);
-    int every_length = 1;
-    vector<int> arr;
-    while (!single_line.empty())
-    {
-        int num = single_line.back()->val;
-        // 判断是否对称
-        if (arr.size() < every_length)
-            arr.push_back(single_line.front()->val);
-        else
-        {
-            while (!arr.empty())
-            {
-                if (num == arr.back())
-                    arr.pop_back();
-                else
-                    return false;
-            }
-        }
-        single_line.push(single_line.front()->left);
-        single_line.push(single_line.front()->right);
-        single_line.pop();
-    }
-    if (!arr.empty())
-        return false;
+    return helper(root->left, root->right);
 }
