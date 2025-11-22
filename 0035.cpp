baf15fd4c17ec4176f417709485b9d67a5260cbb
l-library <borui-li@outlook.com>
2025-11-22 23:59:38 +0800

    problem remain

---
+++ 0035.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+using namespace std;
+
+int searchInsert(vector<int> &nums, int target)
+{
+    if (nums.empty())
+        return 0;
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] > target)
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+    return nums[right] < target ? left : right;
+}
