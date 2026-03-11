9e8dbd9db7abd28d34611fcc630a5bd08519d639
KZlin <borui-li@outlook.com>
2026-03-12 00:06:46 +0800

    2026/3/12

--- Array/0015.cpp
+++ Array/0015.cpp
@@ -34,3 +34,39 @@ vector<vector<int>> threeSum(vector<int> &nums)
     }
     return result;
 }
+
+// 不用set排除重复
+vector<vector<int>> threeSum_1(vector<int> &nums)
+{
+    vector<vector<int>> result;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+        int num = nums[i];
+        int left = i + 1;
+        int right = nums.size() - 1;
+        while (left < nums.size() && right > left)
+        {
+            if (nums[left] + nums[right] > -num)
+                right--;
+            else if (nums[left] + nums[right] < -num)
+                left++;
+            else
+            {
+                result.push_back({num, nums[left], nums[right]});
+                while (left + 1 < right && nums[left + 1] == nums[left])
+                {
+                    left++;
+                }
+                while (right - 1 > left && nums[right - 1] == nums[right])
+                {
+                    right--;
+                }
+                left++;
+            }
+        }
+    }
+    return result;
+}
